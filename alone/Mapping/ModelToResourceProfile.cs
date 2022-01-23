5a77dad16a8229429886eec52cc8d6d89c772747
Mohamed Alsir <mohamed.alsir@halza.com>
2022-01-23 18:07:01 +0400

    Add project files.

---
+++ alone/Mapping/ModelToResourceProfile.cs
@@ -0,0 +1,17 @@
+﻿using alone.Domin;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace alone.Mapping
+{
+    public class ModelToResourceProfile : Profile
+    {
+        public ModelToResourceProfile()
+        {
+            CreateMap<Category, CategoriesResponse>();
+        }
+    }
+}
